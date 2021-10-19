efda9be204e99d8cf508b63398f67928590580c6
pakyeongjo <pakyeongjo@gmail.com>
2021-10-19 16:07:49 +0900

    add params decorator

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
     @Get()
     getAll() {
         return "This will return all movies"
+    }
+
+    @Get('/:id')
+    getOne(@Param('id') id: string){
+        // return "This will return one movies with the id: ${id}";
+        return `This will return one movies with the id: ${id}`;
     }
 }
