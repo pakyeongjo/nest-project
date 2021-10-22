f8a94e04204d19989c7ea3331c586c43f2a5782e
pakyeongjo <pakyeongjo@gmail.com>
2021-10-22 21:33:14 +0900

    dto, extenions

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { retry } from 'rxjs';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -21,31 +23,32 @@ export class MoviesController {
     // }
 
     @Get('/:id')
-    getOne(@Param('id') id: string): Movie{
+    getOne(@Param('id') id: number): Movie{
         // return "This will return one movies with the id: ${id}";
         // return `This will return one movies with the id: ${id}`;
         return this.moviesService.getOne(id)
     }
 
     @Post()
-    create(@Body() movieData){
+    create(@Body() movieData: CreateMovieDto){
         // return 'This will create a movie';
         // return movieData;
         return this.moviesService.create(movieData);
     }
 
     @Delete(":id")
-    remove(@Param('id') id: string){
+    remove(@Param('id') id: number){
         // return `This will delete a movie with the id: ${id}`;
         return this.moviesService.deleteOne(id);
     }
 
     @Patch(":id")
-    patch(@Param('id') id: string, @Body() updateData) {
+    patch(@Param('id') id: number, @Body() updateData: UpdateMovieDto) {
         // return `This will patch a movie with the id: ${id}`;
-        return {
-            updateMovie: id,
-            updateData
-        }
+        // return {
+        //     updateMovie: id,
+        //     updateData
+        // }
+        return this.moviesService.update(id, updateData);
     }
 }
