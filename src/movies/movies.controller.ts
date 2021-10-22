464c214f9e821766d312410a736b2db3fe0a24d7
pakyeongjo <pakyeongjo@gmail.com>
2021-10-22 17:12:47 +0900

    add service and crud with dummy db

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,33 +1,43 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { retry } from 'rxjs';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
+
+    constructor(private readonly moviesService: MoviesService) {}
+
     @Get()
-    getAll() {
-        return "This will return all movies";
+    getAll(): Movie[]{
+        // return "This will return all movies";
+        return this.moviesService.getAll();
     }
 
-    @Get("search")
-    search(@Query("year") searchYear){
-        // return "searching..."
-        return `searching... ${searchYear}`
-    }
+    // @Get("search")
+    // search(@Query("year") searchYear){
+    //     // return "searching..."
+    //     return `searching... ${searchYear}`
+    // }
 
     @Get('/:id')
-    getOne(@Param('id') id: string){
+    getOne(@Param('id') id: string): Movie{
         // return "This will return one movies with the id: ${id}";
-        return `This will return one movies with the id: ${id}`;
+        // return `This will return one movies with the id: ${id}`;
+        return this.moviesService.getOne(id)
     }
 
     @Post()
     create(@Body() movieData){
-        return movieData;
         // return 'This will create a movie';
+        // return movieData;
+        return this.moviesService.create(movieData);
     }
 
     @Delete(":id")
     remove(@Param('id') id: string){
-        return `This will delete a movie with the id: ${id}`;
+        // return `This will delete a movie with the id: ${id}`;
+        return this.moviesService.deleteOne(id);
     }
 
     @Patch(":id")
